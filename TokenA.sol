351c690b45af3d7079e827418989e0ee73c03aa0
Kevin Chibuoyim <54369764+ginakev@users.noreply.github.com>
2022-08-17 21:48:34 +0100

    Update TokenA.sol

---
+++ TokenA.sol
@@ -0,0 +1,11 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract MyToken is ERC20 {
+    constructor() ERC20("TokenA", "TOA") {
+        // Mint 10000 tokens to msg.sender
+        _mint(msg.sender, 10000 * 10 ** 18);
+    }
+}
